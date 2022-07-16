5d8912f196b00d4867a96a65f825c764e0f21cd1
mbiman <juniormbi80@gmail.com>
2022-07-16 09:39:05 -0400

    more progress

---
+++ methodOverloading/MethodOverload.cs
@@ -0,0 +1,26 @@
+﻿
+// Overloaded method declarations
+
+public class MethodOverload
+{
+    // test overloaded square methods
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Square of integer 10 is {0}", Square(10));
+        Console.WriteLine("Square of double 2.57 is {0}", Square(2.57));
+    }// end main method
+
+    //square method with int argument
+    public static int Square(int intValue)
+    {
+        Console.WriteLine("Called square with int argument: {0}", intValue);
+        return intValue * intValue;
+    } //end square method with int argument
+
+    //square method with double argument
+    public static double Square (double doubleValue)
+    {
+        Console.WriteLine("Called square with double argument: {0}", doubleValue);
+        return doubleValue * doubleValue;
+    }// end square method with double argument
+}// end class Methodoverload
---
+++ optionalArguments/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//optional argument demonstration with method power
+using System;
+
+class calculatePowers
+{
+    // call power with and without optional arguments
+    public static void Main(string[] args)
+    {
+        Console.WriteLine( "Power(10) = {0}", Power( 10 ) ) ;
+        Console.WriteLine( "Power(2, 10) = {0}", Power( 2, 10 ) );
+    }
+
+    //use iteration to calculate power
+    public static int Power(int baseValue, int exponentValue = 2)
+    {
+        int result = 1; //initialize total
+
+        for (int i = 1; i <= exponentValue; i++)
+            result *= baseValue;
+            return result;
+    }
+}
